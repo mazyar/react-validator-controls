0394950e26c7ddb75cabf2f3e6b8465a65abca8c
Mohsen Mosaddegh <mehrdad.mazyar@gmail.com>
2018-07-17 17:35:10 +0430

    change main index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import { REQUIRED, MOBILE, MOBILE_EMAIL } from "./models";
 import InputValidate from './input.validate';
 import ValidateForm from './form.validate';
 
 export {
     InputValidate,
-    ValidateForm,
-    REQUIRED,
-    MOBILE,
-    MOBILE_EMAIL
+    ValidateForm
 }
